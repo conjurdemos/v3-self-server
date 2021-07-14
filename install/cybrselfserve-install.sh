0e69290b2c60535a76996aa496b4487cd706f25e
Jody Hunt <jodyhunt@hotmail.com>
2021-07-14 13:30:59 -0500

    Update cybrselfserve-install.sh

--- install/cybrselfserve-install.sh
+++ install/cybrselfserve-install.sh
@@ -8,18 +8,25 @@ main() {
   install-java
   install-tomcat
   install-mysql8
-  sudo apt install -y git ant curl jq python3-tk
+  sudo apt install -y git ant curl jq
   echo "Installation complete."
   echo "To configure & test:"
-  echo "  1) cd ~/v3-self-server"
+  echo "  1) cd .."
   echo "  2) edit cybrselfserve.properties with your PAS & Conjur config details"
   echo "  3) run: ./ant.sh publish"
-  echo "  4) cd mysql/appgovdb"
-  echo "  5) run: ./1-create-db.sh"
-  echo "  6) run: ./2-db-load.sh db_load_appgovdb.sql"
-  echo "  7) run: ./3-db-query.sh dump_all.sql"
   echo "  8) cd ../../servlet-tests"
-  echo "  9) run: gui-lifecycle"
+  echo "  9) run: liveness-test.sh"
+  echo
+  echo "If all is configured properly you should see output similar to below:"
+  echo
+  echo "unapproved returned:"
+  echo "{\"unapproved\": []}"
+  echo
+  echo "paslogin returned:"
+  echo "{\"pasSessionToken\": \"ZTY1ZmRiZGUtZmQ5My00ZWI5LThlNDEtYWJiMzFiOWI2OWVjOzgyRjA4NDJENDNBRkE0QUUwRTRGOTQ0RDhCNDJFREM5NTlGRjI2QkFDRTY5NEFDN0VCM0Y2QjNERTM2QUQyQUM7MDAwMDAwMDJCMUREREVBNzJGNEYyRDIzRURCRTcxNjBFMkQ2RThGQkNBMTQ0NUVDQjU1NkUyRjY4QUFDQkJBRkExRkY5ODAyMDAwMDAwMDA7\"}"
+  echo 
+  echo "conjurlogin returned:"
+  echo "{\"conjurAccessToken\": \"eyJwcm90ZWN0ZWQiOiJleUpoYkdjaU9pSmpiMjVxZFhJdWIzSm5MM05zYjNOcGJHOHZkaklpTENKcmFXUWlPaUkxWkRKalpXTXlaR1EyWVRCa1lqQmpZelUwTkdJeFpETm1ZbUUyWldKaE9USmlNRFkzTUdKalpUSXdPRGN4TldWak5USmpaakV5WWpSbFlUWmhNRFF3SW4wPSIsInBheWxvYWQiOiJleUp6ZFdJaU9pSmhaRzFwYmlJc0ltbGhkQ0k2TVRZeU5qSTROalU1TjMwPSIsInNpZ25hdHVyZSI6Ik5PZVVaNHN4SjQ0dWtyQTZFeFhZc2hlQnc2X18xMzJ4a0Z0Nm4wdUZYeld6aGotNGRYSGVVdkF6VmRDY0JRc0xuVzN3NzgtRTZ3Y2dnWmdyUVBKNS1vdGZVdlQxTmg5SGhnbmMtRkQ5eGpZOTdpRHZMeTBmREJIZUpUUll6NTZ5Z3BFZ1o1U0xjMU9Yb0NnNmw0N3NmeVU2Z2U1SVRmSERJWW9xZW5jak8yVlFhd0pUSVhObUhmMjIzMnRfcjRiZ3dZT1lfNHRzWjFLQnJMTjhjbkhXM0FlNURqTnhKdDVlcVlDeUVpN0tSY2hIa2wwakpmOHF4U21HZGZia1lFSHIxQzJmODJPVlo0YzBZVzVSNmVnM1ZSeVJuUUV0YVhBUVNVakEtSGRDZy1tUXoyOU9lcHR5alIyRUxvZmhxaHhjdTdGU1BRT2tSS25tZUVuYVdaWEZOcF9qc2pKYkg1WUxuWkdoemJqU2Ywd2d6clQwYnEzNU1rdzgyb0JDLVBhYyJ9\"}"
 }
 
 #############################
@@ -93,22 +100,8 @@ install-mysql8() {
   sudo apt-get update -y
   sudo apt-get install -y mysql-server
   sudo mysql_secure_installation
-  sudo service mysql stop
-  while [[ "$(ps -aux | grep mysql | grep -v grep)" != "" ]]; do
-    sleep 3
-  done
-  sudo mkdir -p /var/run/mysqld; sudo chown mysql:mysql /var/run/mysqld
-  sudo mysqld_safe --skip-grant-tables &
-  echo
-  echo
-  echo "waiting 10 seconds for db to initialize..."
-  sleep 10
-  sudo mysql --user=root \
-    -e "UPDATE mysql.user SET authentication_string=null WHERE user='root'; FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Cyberark1'; FLUSH PRIVILEGES;"
-  for i in $(ps -aux | grep mysql | grep -v grep | awk '{print $2}'); do
-    sudo kill -9 "$i"
-  done
-  sudo service mysql start
+  cd ../mysql/appgovdb
+  ./1-create-db.sh
 }
 
 main "$@"
