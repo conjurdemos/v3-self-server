e31309d532c792f6c97fc0c883e88d7f6f652116
Jody Hunt <jody.hunt@cyberark.com>
2021-07-15 18:05:15 -0500

    checkpoint commit

--- mysql/appgovdb/accts_by_id.sql
+++ mysql/appgovdb/accts_by_id.sql
@@ -1,9 +1,9 @@
 SELECT appid.name, ca.resource_type, ca.resource_name, ca.username, sf.name
 FROM appidentities appid, accessrequests ar, safes sf, cybraccounts ca
 WHERE ar.provisioned AND NOT ar.revoked
-AND ar.project_id = 1
-AND appid.project_id = 1
-AND appid.id = 1
-AND ar.app_id = 1
+AND ar.project_id = 4
+AND appid.project_id = 4
+AND appid.id = 4
+AND ar.app_id = 4
 AND ar.safe_id = ca.safe_id
-AND ca.safe_id = sf.id;
+AND sf.id = ca.safe_id ;
