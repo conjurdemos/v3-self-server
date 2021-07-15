80a25b298ece188549d59c84ab4b0ede55ff2bb9
Jody Hunt <jody.hunt@cyberark.com>
2021-07-15 17:27:33 -0500

    checkpoint commit

--- bin/cybrselfserve-install.sh
+++ bin/cybrselfserve-install.sh
@@ -4,18 +4,21 @@
 export TOMCAT_MINOR_VERSION=50
 
 main() {
+  install-mysql8	# run this first cuz it requires user input
+  init-appgovdb
   install-ssh
   install-java
   install-tomcat
-  install-mysql8
   sudo apt install -y git ant curl jq
   echo "Installation complete."
+  echo
   echo "To configure & test:"
-  echo "  1) cd .."
+  echo "  1) cd ../build"
   echo "  2) edit cybrselfserve.properties with your PAS & Conjur config details"
   echo "  3) run: ./ant.sh publish"
-  echo "  8) cd ../../servlet-tests"
-  echo "  9) run: liveness-test.sh"
+  echo "  4) cd ../servlet-tests"
+  echo "  5) run: liveness-test.sh"
+  echo
 }
 
 #############################
@@ -51,7 +54,7 @@ install-tomcat() {
   cp ./tomcat-users.xml /usr/local/tomcat9/conf/tomcat-users.xml
 
   # copy mysql jdbc connector to tomcat lib before starting (saves a restart)
-  cp ../dependencies/mysql-connector-java-8.0.25.jar /usr/local/tomcat9/lib
+  cp ../build/dependencies/mysql-connector-java-8.0.25.jar /usr/local/tomcat9/lib
 
   # start tomcat
   cd /usr/local/tomcat9/bin
@@ -60,37 +63,23 @@ install-tomcat() {
 }
 
 #############################
-install-mysql5() {
-  sudo apt update -y
-  sudo apt install -y mysql-server
-  sudo service mysql stop
-  while [[ "$(ps -aux | grep mysql | grep -v grep)" != "" ]]; do
-    sleep 3
-  done
-  sudo mkdir -p /var/run/mysqld; sudo chown mysql:mysql /var/run/mysqld
-  sudo mysqld_safe --skip-grant-tables &
-  echo
-  echo
-  echo "waiting 10 seconds for db to initialize..."
-  sleep 10
-  ######## MYSQL 5.x
-  sudo mysql --user=root \
-    -e "UPDATE mysql.user SET authentication_string=PASSWORD('Cyberark1') WHERE user='root'; UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root'; FLUSH PRIVILEGES;"
-  for i in $(ps -aux | grep mysql | grep -v grep | awk '{print $2}'); do
-    sudo kill -9 "$i"
-  done
-  sudo service mysql start
-}
-
-#############################
 install-mysql8() {
-  wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.17-1_all.deb
-  sudo dpkg -i mysql-apt-config_0.8.17-1_all.deb
+  MYSQL_DEB_PKG=mysql-apt-config_0.8.17-1_all.deb
+  wget -c https://dev.mysql.com/get/$MYSQL_DEB_PKG
+  sudo dpkg -i $MYSQL_DEB_PKG
+  rm $MYSQL_DEB_PKG
   sudo apt-get update -y
   sudo apt-get install -y mysql-server
   sudo mysql_secure_installation
-  cd ../mysql/appgovdb
+}
+
+#############################
+init-appgovdb() {
+  echo "Initializing appgovdb..."
+  pushd ../mysql/appgovdb
   ./1-create-db.sh
+  ./2-load-db.sh load_appgovdb.sql
+  popd
 }
 
 main "$@"
--- mysql/appgovdb/accts_by_id.sql
+++ mysql/appgovdb/accts_by_id.sql
@@ -3,7 +3,7 @@ FROM appidentities appid, accessrequests ar, safes sf, cybraccounts ca
 WHERE ar.provisioned AND NOT ar.revoked
 AND ar.project_id = 1
 AND appid.project_id = 1
-AND appid.id = 2
-AND ar.app_id = 2
+AND appid.id = 1
+AND ar.app_id = 1
 AND ar.safe_id = ca.safe_id
 AND ca.safe_id = sf.id;
